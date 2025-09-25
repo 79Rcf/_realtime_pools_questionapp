743f49b7bfa3057a118d909759b7c9d6bc50f670
ndang11 <royaltyhopsin@gmail.com>
2025-09-25 13:57:45 +0100

    problem solving

--- Frontend/vite-project/src/pages/HomePage/Home.jsx
+++ Frontend/vite-project/src/pages/HomePage/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <h1>🎉 Welcome to Real-Time Polls</h1>
+      <h1>🎉 Welcome to Real-Time Polls....</h1>
       <p>Create and join interactive polls in real time.</p>
       <div style={{ marginTop: "20px" }}>
         {/* <Link to="/register"><button>Host a Session</button></Link>
--- Frontend/vite-project/src/pages/JoinSessionPage/JoinSession.jsx
+++ Frontend/vite-project/src/pages/JoinSessionPage/JoinSession.jsx
@@ -37,7 +37,7 @@ const JoinSession = () => {
 
   return (
     <div>
-      <h2>Join a Session</h2>
+      <h2>Join a Session..</h2>
 
       {!isHost ? (
         <>
